1a3c301887f3f9fc51b2c5691315e40f3a737acb
amirsaman <amirsaman.gord@gmail.com>
2019-08-30 19:42:08 +0430

    test commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,35 @@
 const testModules = require('./test-module');
 require('../css/app.css');
+require('../css/style.css');
+var SimpleCrypto = require("simple-crypto-js").default;
+var $ = require("jquery");
 
 /********** Paste your code here! ************/
 
 console.log('Paste your code here!');
 console.log(testModules.hello);
+
+
+// If you would like to generate a random unique key, you may use static function generateRandom() like so
+// var _secretKey = SimpleCrypto.generateRandom();
+// You may also set the strength of the random key, as example 256 (default is 128);
+// var _secretKey = SimpleCrypto.generateRandom(256);
+// Or just defined the key by yourself (key is must!)
+var _secretKey = "some-unique-key";
+
+var simpleCrypto = new SimpleCrypto(_secretKey);
+
+var plainText = "Hello World!";
+var chiperText = simpleCrypto.encrypt(plainText);
+console.log("Encryption process...");
+console.log("Plain Text    : " + plainText);
+console.log("Cipher Text   : " + chiperText);
+var decipherText = simpleCrypto.decrypt(chiperText);
+console.log("... and then decryption...");
+console.log("Decipher Text : " + decipherText);
+console.log("... done.");
+
+
+$(document).ready(function () {
+  $('nav').addClass('testJquery');
+})
